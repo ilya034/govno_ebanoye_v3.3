81bc626269631bf4b20c336edd2148f939ecd83c
User39 <wiseevening39@gmail.com>
2025-03-22 14:42:43 +0500

    Delete savelogs.php

--- savelogs.php
+++
@@ -1,8 +0,0 @@
-<?php
-$logFile = 'log.txt';
-
-$ip = $_SERVER['REMOTE_ADDR'];
-$time = date('Y-m-d H:i:s');
-$logEntry = "IP: $ip, Время: $time\n";
-file_put_contents($logFile, $logEntry, FILE_APPEND);
-?>
