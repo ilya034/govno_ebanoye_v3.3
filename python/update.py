bc72e6d527197e6c09ac1d1dde48681d15f0bf74
i5304 <i5304093@gmail.com>
2025-03-26 18:30:36 +0500

    fix workflow

--- python/update.py
+++ python/update.py
@@ -4,11 +4,13 @@ import pymupdf
 import os
 from os import path
 
+HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:136.0) Gecko/20100101 Firefox/136.0', 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8', 'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3', 'Accept-Encoding': 'gzip, deflate, br, zstd', 'Sec-GPC': '1', 'Connection': 'keep-alive', 'Cookie': '__ddg1_=EKc899XJwoULGPq3PAqO', 'Upgrade-Insecure-Requests': '1', 'Sec-Fetch-Dest': 'document', 'Sec-Fetch-Mode': 'navigate', 'Sec-Fetch-Site': 'cross-site', 'DNT': '1', 'Priority': 'u=0, i', 'Pragma': 'no-cache', 'Cache-Control': 'no-cache', 'TE': 'trailers'}
+
 PDF_URL = "https://docs.cspu.ru/raspisanie-zanyatiy/och/fizmat(Inf)/Raspisanie/1k.pdf"
 PDF_FILENAME = "full_schedule.pdf"
 IMG_FULLPATH = path.join(os.getcwd(), "img", "schedule.png")
 
-r = requests.get(PDF_URL)
+r = requests.get(PDF_URL, headers=HEADERS)
 
 if (r.status_code != 200):
     print(f"Error status code: {r.status_code}:\n{r.text}")
